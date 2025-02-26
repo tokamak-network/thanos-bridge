08ac43c61f00ae761becf420d96c8ecff4aaa0c1
hazardvictor <victor@tokamak.network>
2025-02-26 12:19:24 +0100

    fix: filter unsupported token

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,10 +1,24 @@
 import { supportedTokens } from "@/constants/token";
 import { Token } from "@/types/token";
-
+import { l1Chain, l2Chain } from "@/config/network";
 export const getTokenInfoByChainId = (chainId: number): Token[] => {
-  return supportedTokens.filter(
-    (t) => t.chainId === chainId && (t.address || t.isNativeCurrency)
-  );
+  const destinationChainId = chainId === l1Chain.id ? l2Chain.id : l1Chain.id;
+  return supportedTokens.filter((t) => {
+    const destinationToken = supportedTokens.find(
+      (dt) =>
+        dt.chainId === destinationChainId &&
+        dt.bridgedTokenSymbol === t.bridgedTokenSymbol
+    );
+    if (
+      t.chainId === chainId &&
+      ((t.address && destinationToken?.address) ||
+        t.isNativeCurrency ||
+        destinationToken?.isNativeCurrency)
+    ) {
+      return true;
+    }
+    return false;
+  });
 };
 
 export const getTokenInfoByAddress = (
