6cd6fd2be0ae07eb7a17b6b03c3f4eee1bca033e
hazardvictor <victor@tokamak.network>
2025-04-14 10:41:31 +0200

    fix: multi wallet issue

--- src/hooks/bridge/useThanosSDK.tsx
+++ src/hooks/bridge/useThanosSDK.tsx
@@ -7,6 +7,8 @@ import { getChainLayer } from "@/utils/network";
 import { ChainLayerEnum } from "@/types/network";
 import { l1Provider, l2Provider } from "@/constants/provider";
 import { env } from "next-runtime-env";
+import { getEthersSigner } from "@/utils/provider";
+import { config } from "@/config/wagmi.config";
 
 export const useThanosSDK = (l1ChainId: number, l2ChainId: number) => {
   const { isConnected, chain } = useWalletConnect();
@@ -17,40 +19,47 @@ export const useThanosSDK = (l1ChainId: number, l2ChainId: number) => {
     [chain]
   );
   useEffect(() => {
-    if (!l1ChainId || !l2ChainId) return;
-    if (!isConnected) return;
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const l1Contracts = {
-      AddressManager: env("NEXT_PUBLIC_ADDRESS_MANAGER_ADDRESS"),
-      L1CrossDomainMessenger: env(
-        "NEXT_PUBLIC_L1_CROSS_DOMAIN_MESSENGER_ADDRESS"
-      ),
-      L1StandardBridge: env("NEXT_PUBLIC_STANDARD_BRIDGE_ADDRESS"),
-      StateCommitmentChain:
-        "0x0000000000000000000000000000000000000000" as const,
-      CanonicalTransactionChain:
-        "0x0000000000000000000000000000000000000000" as const,
-      BondManager: "0x0000000000000000000000000000000000000000" as const,
-      OptimismPortal: env("NEXT_PUBLIC_OPTIMISM_PORTAL_ADDRESS"),
-      OptimismPortal2: env("NEXT_PUBLIC_OPTIMISM_PORTAL_ADDRESS"),
-      L2OutputOracle: env("NEXT_PUBLIC_L2_OUTPUT_ORACLE_ADDRESS"),
-      L1UsdcBridge: env("NEXT_PUBLIC_L1_USDC_BRIDGE_ADDRESS"),
-      DisputeGameFactory: env("NEXT_PUBLIC_DISPUTE_GAME_FACTORY_ADDRESS"),
+    const init = async () => {
+      try {
+        if (!l1ChainId || !l2ChainId) return;
+        if (!isConnected) return;
+        const signer = await getEthersSigner(config);
+        const l1Contracts = {
+          AddressManager: env("NEXT_PUBLIC_ADDRESS_MANAGER_ADDRESS"),
+          L1CrossDomainMessenger: env(
+            "NEXT_PUBLIC_L1_CROSS_DOMAIN_MESSENGER_ADDRESS"
+          ),
+          L1StandardBridge: env("NEXT_PUBLIC_STANDARD_BRIDGE_ADDRESS"),
+          StateCommitmentChain:
+            "0x0000000000000000000000000000000000000000" as const,
+          CanonicalTransactionChain:
+            "0x0000000000000000000000000000000000000000" as const,
+          BondManager: "0x0000000000000000000000000000000000000000" as const,
+          OptimismPortal: env("NEXT_PUBLIC_OPTIMISM_PORTAL_ADDRESS"),
+          OptimismPortal2: env("NEXT_PUBLIC_OPTIMISM_PORTAL_ADDRESS"),
+          L2OutputOracle: env("NEXT_PUBLIC_L2_OUTPUT_ORACLE_ADDRESS"),
+          L1UsdcBridge: env("NEXT_PUBLIC_L1_USDC_BRIDGE_ADDRESS"),
+          DisputeGameFactory: env("NEXT_PUBLIC_DISPUTE_GAME_FACTORY_ADDRESS"),
+        };
+        const cm = new thanosSDK.CrossChainMessenger({
+          bedrock: true,
+          contracts: {
+            l1: l1Contracts,
+          },
+          l1ChainId: l1ChainId,
+          l2ChainId: l2ChainId,
+          l1SignerOrProvider:
+            chainLayer === ChainLayerEnum.L1 ? signer : l1Provider,
+          l2SignerOrProvider:
+            chainLayer === ChainLayerEnum.L2 ? signer : l2Provider,
+          nativeTokenAddress: env("NEXT_PUBLIC_NATIVE_TOKEN_L1_ADDRESS"),
+        });
+        setCrossChainMessenger(cm);
+      } catch (error) {
+        console.error("Error initializing Thanos SDK:", error);
+      }
     };
-    const cm = new thanosSDK.CrossChainMessenger({
-      bedrock: true,
-      contracts: {
-        l1: l1Contracts,
-      },
-      l1ChainId: l1ChainId,
-      l2ChainId: l2ChainId,
-      l1SignerOrProvider:
-        chainLayer === ChainLayerEnum.L1 ? provider.getSigner() : l1Provider,
-      l2SignerOrProvider:
-        chainLayer === ChainLayerEnum.L2 ? provider.getSigner() : l2Provider,
-      nativeTokenAddress: env("NEXT_PUBLIC_NATIVE_TOKEN_L1_ADDRESS"),
-    });
-    setCrossChainMessenger(cm);
+    init();
   }, [l1ChainId, l2ChainId, chain, isConnected, chainLayer]);
 
   const estimateGas = useMemo(() => {
